5a1839a7808ab99c12c82ed87e3218c24402f18e
jayapriyathimmappa <114154874+jayapriyathimmappa@users.noreply.github.com>
2022-09-22 17:12:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+import Categories from './component/Categories';
+import items from './component/Data';
+import Menu from './component/Menu';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
